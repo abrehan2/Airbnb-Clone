0b3dd7b63d9cd715c822b20ad718190c8e33574f
abrehan2 <rehannajam2@gmail.com>
2023-08-11 02:45:45 +0500

    initial commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// IMPORTS -
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height={30}
+      width={30}
+      alt="Avatar"
+      src={"/images/placeholder.jpg"}
+    />
+  );
+};
+
+export default Avatar;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,6 +2,7 @@
 
 // IMPORTS -
 import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
 
 const UserMenu = () => {
   return (
@@ -19,6 +20,9 @@ const UserMenu = () => {
             className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
           >
             <AiOutlineMenu />
+            <div className="hidden md:block">
+              <Avatar />
+            </div>
           </div>
         </div>
       </div>
