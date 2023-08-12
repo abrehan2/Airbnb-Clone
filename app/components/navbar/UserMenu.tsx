552e8cd98926c27633d5d4fcfda024bf626c1c20
abrehan2 <rehannajam2@gmail.com>
2023-08-13 01:28:32 +0500

    initial commit

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,8 +3,16 @@
 // IMPORTS -
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <>
       <div className="relative">
@@ -16,7 +24,7 @@ const UserMenu = () => {
             Travel App your home
           </div>
           <div
-            onClick={() => {}}
+            onClick={toggleOpen}
             className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
           >
             <AiOutlineMenu />
@@ -25,6 +33,17 @@ const UserMenu = () => {
             </div>
           </div>
         </div>
+
+        {isOpen && (
+          <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+            <div className="flex flex-col cursor-pointer">
+              <>
+                <MenuItem onClick={() => {}} label="Login" />
+                <MenuItem onClick={() => {}} label="Sign up" />
+              </>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
