7e6d17de0a823e0994b58ddf84d6e3383353932b
abrehan2 <rehannajam2@gmail.com>
2023-08-13 23:07:37 +0500

    initial commit

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,23 @@
-const Modal = () => {
+"use client";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryLabel
+}) => {
   return (
-    <div>Modal</div>
+    <div></div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal isOpen/>
           <Navbar />
         </ClientOnly>
         {children}
