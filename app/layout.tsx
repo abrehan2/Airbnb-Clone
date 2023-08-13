16ab942872b4a67a7a640180d8d1b8431c924f00
abrehan2 <rehannajam2@gmail.com>
2023-08-14 02:23:31 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen/>
+          <Modal isOpen={true}/>
           <Navbar />
         </ClientOnly>
         {children}
