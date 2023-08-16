411f289acab10eb8ae8460a78ae232e890480136
abrehan2 <rehannajam2@gmail.com>
2023-08-17 02:10:31 +0500

    initial commit

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,8 +1,17 @@
+"use client";
 
+// IMPORTS -
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import {FcGoogle} from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "../hooks/useRegisterModal";
 
 const RegisterModal = () => {
   return (
-    <div>RegisterModal</div>
+   <>
+   </>
   )
 }
 
