15b66539ef8d7e0a2b6cdc0a565f52d311445aa0
abrehan2 <rehannajam2@gmail.com>
2023-08-24 20:37:34 +0500

    initial commit

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const client = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = client;
+}
+
+export default client;
