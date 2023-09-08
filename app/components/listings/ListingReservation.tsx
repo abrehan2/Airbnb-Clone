645b23654310df51a3610c491e9aabf202d2ab18
abrehan2 <rehannajam2@gmail.com>
2023-09-09 02:14:13 +0500

    Setting up listing reservation

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,9 +1,55 @@
+"use client";
 
+// npm i react-date-range
+// npm i -D @types/react-date-range
 
-const ListingReservation = () => {
-  return (
-    <div>ListingReservation</div>
-  )
+// IMPORTS -
+import { Range } from "react-date-range";
+
+// PARTIALS -
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
 }
 
-export default ListingReservation
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  dateRange,
+  totalPrice,
+  onChangeDate,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div
+      className="
+    bg-white
+    rounded-xl
+    border-[1px]
+    border-neutral-200
+    overflow-hidden"
+    >
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+     <Calendar
+     value={dateRange}
+     disabledDates={disabledDates}
+     onChange={(value) => onChangeDate(value.selection)}
+     
+     />
+       
+    </div>
+  );
+};
+
+export default ListingReservation;
