6154b0e6396c4a4004339e8e38ef7554f07c053a
abrehan2 <rehannajam2@gmail.com>
2023-09-14 03:13:37 +0500

    Implemented FavoriteClient.tsx

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,14 +2,27 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoriteClient from "./FavoriteClient";
 
 const ListingPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
-      <EmptyState
-        title="No favorites found"
-        subtitle="Looks like you have no favorite listings."
-      />
+      <FavoriteClient listings={listings} currentUser={currentUser} />
     </ClientOnly>
   );
 };
